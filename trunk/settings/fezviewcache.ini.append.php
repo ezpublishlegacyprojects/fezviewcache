4b4db3f4f2c8554dad1ca1488620041a1151adea
frefred34 <info@example.com>
2008-10-02 12:35:24 +0000

    Settings file added
    git-svn-id: http://svn.projects.ez.no/fezviewcache@2 43dc41ba-fba1-43fe-af48-3b54643cf7bb

---
+++ trunk/settings/fezviewcache.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+
+[CacheMethod]
+ClearCacheMethod[]
+ClearCacheMethod[]=object
+ClearCacheMethod[]=parent
+ClearCacheMethod[]=relating
+ClearCacheMethod[]=keyword
+ClearCacheMethod[]=siblings
+ClearCacheMethod[]=all
+
+*/ ?>
